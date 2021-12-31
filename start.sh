7675aa9b746119af8b2ca47ee745f59cebfc8bcc
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-12-31 23:04:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 wget https://codeload.github.com/aashath0317/TorrentLeechX/zip/refs/heads/master
 unzip master
 mv TorrentLeechX-master/* /usr/src/app/
-ls
-CMD ["bash","start.sh"]
+if [[ -n $RCLONE_CONFIG ]]; then
+ echo "Rclone config detected"
+ echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+fi
+
+bash setup.sh; python3 -m tobrot
