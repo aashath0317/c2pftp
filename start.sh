91816f35bb3c74cc0fc2460db6099466daec5454
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-12-31 22:56:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 wget https://codeload.github.com/aashath0317/TorrentLeechX/zip/refs/heads/master
 unzip master
-mv TorrentLeechX-master/* ..
+mv TorrentLeechX-master/* /usr/src/app/
 CMD ["bash","start.sh"]
