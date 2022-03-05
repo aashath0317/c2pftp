312c33b861edf0694f40a298dceb01de8c1afdd3
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-03-05 11:20:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 wget https://codeload.github.com/aashath0317/ftptest/zip/refs/heads/master
 unzip master
 mv ftptest-master/* /usr/src/app/
+unzip accounts.zip
 
 python3 update.py && python3 -m bot
