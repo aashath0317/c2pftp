57976a874d57a5f4dc1e059fa9950bfb4d48e7e2
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-03-05 09:25:46 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 wget https://codeload.github.com/aashath0317/ftptest/zip/refs/heads/master
 unzip master
 mv ftptest-master/* /usr/src/app/
-mv rclone.conf '/usr/src/app/.config/rclone/rclone.conf'
 unzip accounts.zip
 
 python3 update.py && python3 -m bot
